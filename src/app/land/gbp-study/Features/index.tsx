aebd3515842d2cf951601dea15c6fa564e652adf
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-11-27 05:35:24 +0900

    Update gbp-study landing page

--- src/app/land/gbp-study/Features/index.tsx
+++ src/app/land/gbp-study/Features/index.tsx
@@ -3,175 +3,225 @@ import React from "react"
 import Image from "next/image"
 import SectionHeader from "@/components/Common/SectionHeader"
 import Join from "../Join"
+import Link from "next/link"
 
 const Feature = () => {
   return (
     <>
       {/* <!-- ===== Features Start ===== --> */}
-      <section id="features" className="py-4">
-        <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
+      <section id="features" className="py-4 mx-2 md:mx-12 xl:mx-24">
+        <div className="mx-2">
+          <h2 className="mx-6 my-12 text-center text-bold text-base md:text-3xl">
+            <div className="my-2">
+              なぜ、資金ゼロ、知識ゼロ、HPすら持っていない店舗オーナーが
+            </div>
+            <div className="my-2">
+              Google検索１位を実現し、圧倒的な集客を実現できたのか？
+            </div>
+            <div className="my-4">理由は３つあります。</div>
+          </h2>
+          <div className="mx-2 md:mx-12 mb-12 flex flex-nowrap gap-2 md:gap-10 items-stretch justify-between">
+            <div className="bg-slate-300 text-black text-lg text-justify rounded-xl w-1/3 p-2 md:p-4">
+              <h3 className="mb-2 text-sm md:text-xl text-center">理由１</h3>
+              <h4 className="my-2 text-sm md:text-xl">
+                １位表示されると段違いにクリック率が高くなるから
+              </h4>
+              <ul className="list-none p-0 mx-0 md:px-1 md:mx-1 my-2 text-xs md:text-xl text-center">
+                <li className="mb-0 text-xs md:text-xl">１位…21.12%</li>
+                <li className="mb-0 text-xs md:text-xl">２位…10.65%</li>
+                <li className="mb-0 text-xs md:text-xl">３位… 7.57%</li>
+              </ul>
+              <div className="text-[9px] leading-[10px] md:text-xl">
+                (Internet Marketing Ninjas社)
+              </div>
+              <ul className="px-1 mx-0 my-2 text-sm md:text-xl list-none text-center">
+                <li className=" text-xs md:text-xl">１位と２位の差は約２倍</li>
+                <li className=" text-xs md:text-xl">１位と３位の差は約３倍</li>
+              </ul>
+              <div className=" text-xs md:text-xl">
+                このクリック率の違いがそのまま集客力の違いに現れます
+              </div>
+            </div>
+            <div className="bg-slate-300 text-black text-justify text-xs md:text-xl text-center rounded-xl w-1/3 p-2 md:p-4">
+              <h3 className="mb-2 text-sm md:text-xl text-center">理由２</h3>
+              <h4 className="my-2 text-sm md:text-xl">
+                Googleが今最も注力しているツールを導入しているから
+              </h4>
+              <p>
+                Googleは、検索ユーザーに対してより価値のある情報を
+                提供することに全力を尽くしています。
+              </p>
+              <p>
+                そのため、ビジネスオーナーがより良い情報をより
+                簡単に発信できるツールを無償で提供しています。
+              </p>
+              <p>
+                そのGoogleが推奨するツールを利用するので、成果が出るのは当然といえます。
+              </p>
+            </div>
+            <div className="bg-slate-300 text-black text-lg text-center rounded-xl w-1/3 p-2 md:p-4">
+              <h3 className="mb-2 text-sm md:text-xl">理由３</h3>
+              <h4 className="text-justify my-2 text-sm md:text-xl">
+                ツールの使い方を正しく理解し、
+                <span className="md:whitespace-nowrap"> そのとおりに</span>
+                実践しているから
+              </h4>
+              <div className="text-justify text-xs md:text-xl">
+                <p>
+                  どんなに優れた道具であっても、正しい利用法を理解しなければ、よい成果を出すことはできません。
+                </p>
+                <p>
+                  でも、正しい方法を知り、そのとおりに実践すれば、
+                  ほぼ確実に成果を出すことができます。しかも、その方法は驚くほど簡単です。
+                </p>
+                <p>１日たった２，３分の作業でOKです。</p>
+              </div>
+            </div>
+          </div>
+
           {/* <!-- Section Title Start --> */}
           <SectionHeader
             headerInfo={{
-              title: "みんなが求めているのは",
+              title: "その秘訣は？",
               subtitle: (
-                <div className="text-center">
-                  <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                    新しい自分
-                  </span>
-                  になる方法
+                <div className="text-center md:mx-2 text-xl md:text-3xl">
+                  <div> でも、どうやってやるの？</div>
+                  <div>その方法はこのまま続きをご覧ください</div>
                 </div>
               ),
               description: (
                 <div className="w-fit mx-auto my-10">
-                  <p>地球上には、７０億人の人がいます。</p>
-                  <p>日本だけでも、１億２３３０万人の人がいます。</p>
+                  <div className="relative">
+                    <div className="bg-slate-200 rounded-full p-2 px-6 mb-4 w-fit">
+                      <div className="text-sm md:text-xl text-bold">
+                        地域密着でがんばる
+                      </div>
+                      <div className="text-sm md:text-xl text-bold">
+                        店舗オーナーのための集客勉強会
+                      </div>
+                    </div>
+                    <div className="flex flex-col rounded-full bg-orange-600 text-white text-lg md:text-3xl font-bold w-fit items-center justify-center w-[4.5rem] h-[4.5rem] md:w-[8rem] md:h-[8rem] absolute top-[0rem] md:top-[-2rem] right-[0rem]">
+                      <div>参加費</div>
+                      <div>無料</div>
+                    </div>
+                  </div>
+                  <h1 className="mb-5 md:pr-16 text-xl md:text-3xl font-bold text-black dark:text-white xl:text-hero ">
+                    あなたのお店をGoogle１位表示にして
+                    <br />
+                    <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
+                      新規客を大量に
+                    </span>
+                    集める方法
+                  </h1>
                 </div>
               )
             }}
           />
-          <div className="w-full mt-4 md:w-[46em] mx-auto">
-            <p>
-              その人たちはみんな、自分をアップデートして新しい自分になる方法を知りたいと思っています。
-              新しい自分にしてほしいと願っています。
-            </p>
-            <h4 className="text-[16px] sm:text-xl md:text-2xl">
-              私たちが何かを知るための方法は変わりました
-            </h4>
-            <p>
-              ホームページ、ブログ、Instagram、YouTube、そしてGoogleマップから、知りたいことを見つけることができます。
-            </p>
-            <p>あなたは、探している人たちとつながることができるんです。</p>
-            <p>
-              学校の中で友だちに聞いたり、先生に教わったりするよりも、多くのことを学ぶことができます。
-            </p>
-            <p>
-              博士号や、ＭＢＡを取得した人から学ぶこともできます。
-              ほんの数歩だけ先にいっている人からも学ぶことができます。
-            </p>
-            <p>
-              どんな業界でも、どんなトピックでも、自分を変える方法や、変えてくれる場所がどこか知ることができます。
-            </p>
-            <blockquote>
-              <p>フィットネス、ヨガ、不動産仲介、健康、食事、自己啓発・・・</p>
-              <p>
-                アプリの使い方、アプリの作り方、犬のしつけ、ポテトの栽培方法・・・
-              </p>
-            </blockquote>
-            <p>あなたは、これが何を意味するか、理解していますか？</p>
-            <p>
-              あなたが知っていること、あなたがやっていることは、誰かの人生をよりよくする力があるんです。
-            </p>
-            <p>
-              Googleには、質問が何千回も入力されています。<em>１秒間</em>
-              に何千回も入力されているんです。
-            </p>
-            <p>みんながあなたのことを探しています。</p>
-            <p>
-              自分自身を変えるために、あなたの知識とスキルを探しているんです。
-            </p>
-            <p>
-              わたしたちは、自分の知っていること、できることで、
-              誰かの人生を変革できる特別な力を持っているんです。
-            </p>
-            <Image
-              src="/images/about/avatar.png"
-              width={88}
-              height={88}
-              alt="Kiku's avatar"
-              unoptimized
-              className="float-right m-2"
-            />
-            <h3>私の名前は</h3>
-            <p>つつみきくおです。</p>
-            <div className="clear-both" />
-            <p>３年前、わたしはあなたと同じでした。</p>
-            <p>
-              自分が、何の専門家なのかわかりませんでした。色んなことを学んで、色んなことができるようになりました。
-              でも、かえって凡庸な人間にしかなれませんでした。
-            </p>
-            <p>
-              しかし、そこから、自分の強みを見つけることを学んで、自分のビジネスに取り込みました。
-            </p>
-            <p>
-              そして、それを仕事だけでなく私生活にも取り入れるようになりました。そして、素晴らしい力を得ることができたのです。
-            </p>
-            <h4>学んだことを分かち合いたい</h4>
-            <p>
-              それをみんなと分かち合いたいと考えました。世の中にフィードバックしたいと考えたんです。
-            </p>
-            <p>
-              でも、それを探している適切な人に見つけてもらおうとした時、思ったよりもずっと難しいことに気づきました。
-              思ったよりも大変だったんです。
-            </p>
-            <h4>Googleビジネスプロフィールにたまたま出会った</h4>
-            <p>
-              でも、Googleビジネスプロフィールに出会いました。自分で学んで、友人のネイルサロンで代わりに運用してみました。
-            </p>
-            <h5>ネイルサロンでは・・・１週間で新規獲得</h5>
-            <p>
-              彼女のサロンは、２年間も新規客が来ず、その上コロナの影響でどんどん先細りになっていました。
-            </p>
-            <p>
-              ところが、Googleビジネスプロフィールを設定してから、たった１週間で新規客が彼女のサロンを見つけてくれたのです。
-            </p>
-            <h5>
-              治療院では・・・１週間でアクセス数3,000件、３ヶ月で100,000件
-            </h5>
-            <p>
-              ある治療院は、設定したビジネス情報が、Googleのガイドラインを違反していました。
-            </p>
-            <p>
-              そこで、その治療院の ビジネス情報を一箇所修正したところ、
-              たった１週間でアクセス数が3,000回に増えました。
-              その後、３ヶ月でアクセス数が100,000回を超えました。
-            </p>
-            <p>
-              私は、わたしのこの知識とスキルを、共有するようになりました。これは、他の専門家たちと一緒になし得たものです。
-            </p>
-            <p>何度も何度も挑戦しては、失敗してきた人たちと一緒にです。</p>
-            <h5>今回の勉強会では</h5>
-            <p>私がこれまで学んだことをパッケージ化して提供するものです。</p>
-            <p>
-              この勉強会では、どのようにすればあなたの知識とスキルを、
-              探している適切な人に見つけてもらえるのかを学ぶことができます。
-            </p>
-            <h4>
-              あなたの知識とスキルが、みんなに与えるインパクトは計り知れません。
-            </h4>
-            <blockquote>
-              一生に一度、特別な瞬間が訪れる。運命に、肩を叩かれるときが来る。
-            </blockquote>
-            <p>チャーチルの言葉です。</p>
-            <p>
-              やってくる特別な瞬間に、準備すべきことをしていなかったら？もし、あなたのことが見つからなくて、
-              あなたのアドバイスも、サービスも受けることができなかったとしたら？
-            </p>
-            <p>
-              その人たちの最高の瞬間になったかもしれないのに、あなたを見つけることができなかったばっかりに・・・、
-              その瞬間を逃してしまいます。
-            </p>
-            <p>
-              今、わたしはあなたに、この勉強会のためのスライドをPDFにしてお送りします。LINEの友だちになって、ダウンロードしてください。
-            </p>
-            <p>今すぐダウンロードして、勉強会の予習を始めて下さい。</p>
-            <h4>この数年で学んだ最も貴重な気付きがあります。</h4>
-            <p>
-              それは、 「<strong>運</strong>」
-              は人間の力ではどうすることもできない。 でも、 「
-              <strong>縁</strong>」
-              なら人間の力で作ることも変えることもできるということです。
-            </p>
-            <p>
-              あなたがこのページを見つけたのもなにかの<strong>縁</strong>です。
-            </p>
-            <p>勉強会でお会いしましょう。</p>
+          <div className="md:w-1/2 mx-2 md:mx-auto text-justify">
+            <h2 className="text-lg md:text-2xl">驚きの実績公開</h2>
+            <ul className="list-outside my-1 bg-orange-100 rounded-xl p-4 px-8">
+              <li className="text-sm md:text-xl">
+                <div className="font-bold">
+                  導入後１ヶ月で５０件のアクセス！
+                </div>
+                <div>２件の電話問い合わせ（印鑑・印章店）</div>
+              </li>
+              <li className="text-sm md:text-xl">
+                <div className="font-bold">
+                  導入３ヶ月ほどで毎週１件の電話問い合わせ！！
+                </div>
+                <div>産業用中古機器販売：１取引あたり単価数百万〜数千万円</div>
+              </li>
+              <li className="text-sm md:text-xl">
+                <div className="font-bold">
+                  導入後２１日間で３１回のアクセス！
+                </div>
+                <div>２件の問い合わせ（食品小売店）</div>
+              </li>
+            </ul>
           </div>
-          {/* <!-- Section Title End --> */}
+
+          <div className="w-full mt-4 md:w-[46em] mx-auto">
+            <h3 className="my-12 mb-4">
+              この勉強会であなたが得られる
+              <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
+                ３つのメリット
+              </span>
+            </h3>
+            <div className="flex flex-nowrap gap-1 items-center justify-between">
+              <div className="flex flex-col rounded-full bg-orange-600 text-white text-xs md:text-xl font-bold w-fit items-center justify-center w-[7rem] h-[7rem] md:w-[14rem] md:h-[14rem] ">
+                <div>1. コストゼロで</div>
+                <div>受講日当日に</div>
+                <div>施策を導入できる</div>
+              </div>
+              <div className="flex flex-col rounded-full bg-orange-600 text-white text-xs md:text-xl font-bold w-fit items-center justify-center  w-[7rem] h-[7rem] md:w-[14rem] md:h-[14rem]">
+                <div>2. 割引なしで</div>
+                <div>安定的に</div>
+                <div>新規客が集まる</div>
+              </div>
+              <div className="flex flex-col rounded-full bg-orange-600 text-white text-xs md:text-xl font-bold w-fit items-center justify-center  w-[7rem] h-[7rem] md:w-[14rem] md:h-[14rem] ">
+                <div>3. 高い確率で</div>
+                <div>リピートに繋がる</div>
+                <div>優良顧客が集まる</div>
+              </div>
+            </div>
+            <h3 className="text-sm md:text-xl p-4 bg-slate-200 rounded-full mt-12">
+              Q. なぜ、コストゼロ・業者不要なのにこんな成果が出るの？
+            </h3>
+            <p className="text-sm md:text-base">
+              A. はい！理由は以下の３つ！！
+            </p>
+            <ul className="my-2">
+              <li className="text-sm md:text-base">
+                ネット業界最大手の起業が完全無償で提供しているサービスだから
+              </li>
+              <li className="text-sm md:text-base">
+                その大手が研究した検索ユーザーの行動心理に基づいた手法だから
+              </li>
+              <li className="text-sm md:text-base">
+                なのにマニアック過ぎて同じネット業界ですらほぼ知られていないから
+              </li>
+            </ul>
+            <h3 className="text-sm md:text-xl p-4 bg-slate-200 rounded-full">
+              Q. 導入することでのデメリットはありますか？
+            </h3>
+            <p className="text-sm md:text-base">
+              A.
+              はい！多くの事業者が知らないことが成功要因の１つなのに、あまりに導入が簡単すぎてライバルも参入しやすく、後発組になると効果が鈍ります！！
+            </p>
+            <h3 className="text-sm md:text-xl p-4 bg-slate-200 rounded-full">
+              Q. どうして、そんな情報を無料で公開するんですか？
+            </h3>
+            <p className="text-sm md:text-base">
+              A.
+              はい！私の利便性のためです😁！より良いお店や会社が検索上位に表示されないと、地元の水戸市に住む私としては非常に不便だからです！！
+            </p>
+            <div>
+              <Image
+                src="/images/user/avatar.png"
+                width={90}
+                height={90}
+                alt="avatar"
+                className="float-right ml-4"
+              />
+              <h3>講師</h3>
+              <div>堤紀久夫</div>
+              <div>
+                元インターネット関係のプログラマ。
+                <Link
+                  href="https://instagram.com/alizza.ideal"
+                  className="underline text-blue-600"
+                >
+                  Instagram
+                </Link>
+                などで、Googleマップとホームページを
+                組み合わせたネット集客の情報を発信しています。
+              </div>
+              <div>（Alizza Ideal 代表)</div>
+            </div>
+          </div>
         </div>
       </section>
-
-      {/* <FeaturesDetail /> */}
 
       {/* <!-- ===== Features End ===== --> */}
     </>
--- src/app/land/gbp-study/Hero/index.tsx
+++ src/app/land/gbp-study/Hero/index.tsx
@@ -17,47 +17,83 @@ const Hero = () => {
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex items-start lg:gap-8 xl:gap-32.5">
             <div className="md:w-1/2 md:mx-auto text-justify">
-              <div className="h4 mb-4.5 text-lg font-medium text-black dark:text-white">
-                Googleビジネスプロフィールを一緒に勉強しましょう。
+              <div className="md:mb-4.5 text-base md:text-2xl font-medium text-black dark:text-white">
+                ホームページすら持っていないのに、まさか、
               </div>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                検索上位表示でみんなが
+              <div className="mb-4.5 text-lg md:text-2xl font-medium text-black dark:text-white">
+                Googleで検索１位が実現できるなんて……
+              </div>
+              <h1 className="mb-5 text-2xl md:text-3xl font-bold text-black dark:text-white xl:text-hero ">
+                そんなこと
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  集まるお店に
+                  無理に
                 </span>
+                決まってる
               </h1>
-              <h2 className="text-2xl">ありがとうございます。</h2>
-              <p>このページにたどり着いてくれて、本当にありがとう。</p>
-              <h3 className="text-xl">
-                あなたがしていることは、素晴らしいことです
-              </h3>
-              <p>
-                あなたが毎日しているのは、どんなことであろうと素晴らしいことなんです。
-                なぜなら、あなたは
-                <ruby>
-                  専門家<rp>(</rp>
-                  <rt>エキスパート</rt>
-                  <rp>)</rp>
-                </ruby>
-                だからです。
-              </p>
-              <h3 className="text-xl">
-                あなたが提供している最も価値のあるもの
-              </h3>
-              <p>
-                あなたが経営している事業ではありません。販売している商品でもありません。
-                サービスしていることでも、あなたが作っている最高の製品でもありません。
-              </p>
-              <p>それは、あなたの知っていること。つまり知識とスキルです。</p>
-              <p>
-                それこそが、あなたが今まで投資してきたなかで、もっとも価値のあるものなんです。
-              </p>
-              <p>
-                血と汗を流しながら試練をかいくぐり、今あなたがいるその場所にたどり着くためにであった障害。
-                経験しなければならなかったすべての障害こそ、あなたが投資してきたことで得た最高のものなんです。
-              </p>
-            </div>
+              <div className="text-base md:text-2xl">
+                って、私もそう思ってました……
+              </div>
+              <div className="text-base md:text-2xl">
+                少なくとも、この現実を目にするまでは……
+              </div>
+              <div className="mb-6 mt-3 md:mt-6 text-base md:text-xl">
+                でも、Googleが提供している、誰でも使える「ある無料ツール」を使うことで、彼らは
+                <span className="font-black"> １位表示を実現</span>
+                しただけでなく、
+                <span className="font-black">こんな成果をいとも簡単に</span>
+                手に入れてしまったのです。
+              </div>
 
+              <div className="flex flex-nowrap">
+                <div className="bg-orange-100 py-2 rounded-l-xl md:rounded-xl text-sm md:text-2xl">
+                  <div className="rounded-full text-white font-bold bg-orange-600 w-fit p-2 px-4 mx-3">
+                    驚きの集客効果‼
+                  </div>
+                  <ul className="list-outside my-4 px-4">
+                    <li className="text-sm md:text-base">
+                      導入後１ヶ月で５０件のアクセス！
+                      <div>２件の電話問い合わせ（印鑑・印章店）</div>
+                    </li>
+                    <li className="text-sm md:text-base">
+                      導入３ヶ月ほどで毎週１件の電話問い合わせ！！
+                      <div>
+                        産業用中古機器販売：１取引あたり単価数百万〜数千万円
+                      </div>
+                    </li>
+                    <li className="text-sm md:text-base">
+                      導入後２１日間で３１回のアクセス！
+                      ２件の問い合わせ（食品小売店）
+                    </li>
+                  </ul>
+                </div>
+                <div className="animate_right block w-2/5 md:hidden">
+                  <div className=" relative aspect-[520/693] w-full">
+                    <Image
+                      className="shadow-solid-l dark:hidden rounded-tr-xl"
+                      src={`/images/hero/gbp-setup-guide-light-half.png`}
+                      alt="GBPビジネス情報設定ガイド"
+                      unoptimized
+                      fill
+                    />
+                    <Image
+                      className="hidden shadow-solid-l dark:block bg-slate-200 rounded-tr-xl"
+                      src={`/images/hero/gbp-setup-guide-dark-half.png`}
+                      alt="GBPビジネス情報設定ガイド"
+                      fill
+                      unoptimized
+                    />
+                  </div>
+                  <p className="my-2 mx-1 text-justify text-xs">
+                    勉強会で使用するPDFは、LINEのお友だちに配布しています。
+                  </p>
+                  <div className="flex flex-wrap gap-5">
+                    <Join>
+                      <span className="text-xs">勉強する</span>
+                    </Join>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="animate_right hidden md:w-1/2 md:block">
               <div className="relative 2xl:-mr-7.5">
                 <Image
@@ -87,26 +123,21 @@ const Hero = () => {
                 <div className=" relative aspect-[700/444] w-full">
                   <Image
                     className="shadow-solid-l dark:hidden"
-                    src="/images/hero/gbp-setup-guide-light.png"
+                    src={`/images/hero/gbp-setup-guide-light.png`}
                     alt="GBPビジネス情報設定ガイド"
                     unoptimized
                     fill
                   />
                   <Image
                     className="hidden shadow-solid-l dark:block bg-slate-200"
-                    src="/images/hero/gbp-setup-guide-dark.png"
+                    src={`/images/hero/gbp-setup-guide-dark.png`}
                     alt="GBPビジネス情報設定ガイド"
                     fill
                     unoptimized
                   />
                 </div>
                 <p className="my-4 text-justify">
-                  LINEでお友だちになってPDFを読んで、
-                  内容に納得がいったら参加表明して下さい。
-                </p>
-                <p>
-                  真剣にGoogleビジネスプロフィールを学んで、
-                  マジでお店を繁盛させたいという方の参加をお待ちしております。
+                  勉強会で使用するPDFは、LINEのお友だちに配布しています。
                 </p>
                 <div className="flex flex-wrap gap-5">
                   <Join>一緒に勉強する</Join>
