acd150ecf84a5f1ad8ed45b07283265502ccfae7
tsntsumi <tsutsumi.kikuo@gmail.com>
2024-01-25 07:51:52 +0900

    Update to simple message follows the book 'Bussiness made simple'

--- sites/blog/src/components/Hero/index.tsx
+++ sites/blog/src/components/Hero/index.tsx
@@ -11,6 +11,12 @@ const Hero = () => {
     e.preventDefault()
   }
 
+  const 集客の錬金術 = () => (
+    <strong className="rounded-full m-1 p-2 py-1 bg-slate-600 text-amber-300 dark:text-amber-600">
+      集客の錬金術
+    </strong>
+  )
+
   const OfferImage = () => {
     return (
       <div className="relative 2xl:-mr-7.5">
@@ -72,9 +78,7 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className="md:w-1/2 text-lg md:mx-auto text-justify">
               <h1 className="text-base">
-                <strong className="rounded-full mx-1 p-2 bg-slate-600 text-amber-300 dark:text-amber-600">
-                  集客の錬金術
-                </strong>
+                <集客の錬金術 />
                 で、あなたの商品をゴールドのように輝かせる
               </h1>
               <h2 className="my-8 pr-16 text-xl font-bold text-black dark:text-white xl:text-hero ">
@@ -90,64 +94,9 @@ const Hero = () => {
                 あなたの商品を<strong>自然と買わせる</strong>
                 ことができるようになります。
               </p>
-              <div className="animate_right block md:hidden">
-                <OfferImage />
-              </div>
-              <h3 className="text-lg">
-                無理に
-                <span className="relative inline-block before:absolute before:bottom-1.5 before:left-0 before:-z-1 before:h-3 before:w-full before:rounded-full before:bg-amber-200 dark:before:bg-amber-600 ">
-                  売り込まなくても
-                </span>
-                売れていく
-              </h3>
-              <p>
-                欲しがっている人だけ集客できるから、
-                セールスで無理な売り込みをして
-                <strong>嫌われる</strong>
-                ことはありません。
-              </p>
-              <h3 className="text-lg">
-                大幅な
-                <span className="relative inline-block before:absolute before:bottom-1.5 before:left-0 before:-z-1 before:h-3 before:w-full before:rounded-full before:bg-amber-200 dark:before:bg-amber-600 ">
-                  値引きをしなくても
-                </span>
-                売れていく
-              </h3>
-              <p>
-                欲しがっている人だけ集客するから、
-                商品の価値と価格を等価交換できるので、 大幅に値引きをして
-                <strong>利益を失う</strong>
-                ことはありません。
-              </p>
-              <h3 className="text-lg">
-                無駄に
-                <span className="relative inline-block before:absolute before:bottom-1.5 before:left-0 before:-z-1 before:h-3 before:w-full before:rounded-full before:bg-amber-200 dark:before:bg-amber-600 ">
-                  説明しなくても
-                </span>
-                売れていく
-              </h3>
-              <p>
-                欲しがっている人だけ集客できるから、 無駄に長々と説明をして
-                <strong>嫌がられる</strong>
-                ことはありません。
-              </p>
-              <h3 className="text-lg">
-                ３ステップ・テンプレートでキャッチコピーを
-                <span className="relative inline-block before:absolute before:bottom-1.5 before:left-0 before:-z-1 before:h-3 before:w-full before:rounded-full before:bg-amber-200 dark:before:bg-amber-600 ">
-                  言語化
-                </span>
-              </h3>
-              <p>
-                アリザ・アイデアルの
-                <strong className="rounded-full m-1 p-2 py-1 bg-slate-600 text-amber-300 dark:text-amber-600">
-                  集客の錬金術
-                </strong>
-                では、 キャッチコピーのテンプレートを使って３ステップで言語化。
-                <strong>だれでも</strong>簡単に売れるキャッチコピーを作れます。
-              </p>
             </div>
 
-            <div className="animate_right hidden md:w-1/2 md:block">
+            <div className="animate_right md:w-1/2">
               <OfferImage />
             </div>
           </div>
