51a31ca93284b5cf296cee17d677dcfaa008e0cb
tsntsumi <tsutsumi.kikuo@gmail.com>
2024-02-06 15:43:22 +0900

    Update menu id

--- sites/blog/src/components/Header/menuData.tsx
+++ sites/blog/src/components/Header/menuData.tsx
@@ -3,51 +3,45 @@ import { Categories } from "@/collections/categories"
 
 const menuData: Menu[] = [
   {
-    id: 1,
-    title: "ホーム",
-    newTab: false,
-    attributes: "",
-    path: "/"
-  },
-  {
-    id: 1.5,
-    title: "更新情報を購読",
+    id: 2,
+    title: "最新情報を購読",
     newTab: false,
     attributes:
-      "flex text-xs rounded-full mx-auto bg-black px-3 whitespace-nowrap py-2 text-white duration-300 ease-in-out hover:bg-amber-500 dark:bg-btndark dark:hover:bg-blackho"
+      "flex text-xs rounded-full mx-auto bg-black px-3 whitespace-nowrap py-2 text-white duration-300 ease-in-out hover:bg-amber-500 dark:bg-btndark dark:hover:bg-blackho",
+    path: "/request/newsletter?category=alchemy"
   },
   {
-    id: 2,
+    id: 3,
     title: "詳しく",
     newTab: false,
     attributes: "hidden md:block",
     submenu: [
       {
-        id: 2.1,
+        id: 3.1,
         title: "キャッチコピーの錬金術とは？",
         newTab: false,
         path: "/#outline"
       },
       {
-        id: 2.2,
+        id: 3.2,
         title: "なぜあなたは集客できないのか？",
         newTab: false,
         path: "/#problem"
       },
       {
-        id: 2.3,
+        id: 3.3,
         title: "どうすればあなたは集客できるのか？",
         newTab: false,
         path: "/#solutions"
       },
       {
-        id: 2.4,
+        id: 3.4,
         title: "あなたはどう集客すればいいのか？",
         newTab: false,
         path: "/#plan"
       },
       {
-        id: 2.5,
+        id: 3.5,
         title: "なぜあなたは集客できるようになるのか？",
         newTab: false,
         path: "/#faq"
@@ -55,37 +49,37 @@ const menuData: Menu[] = [
     ]
   },
   {
-    id: 3,
+    id: 4,
     title: "ブログ",
     newTab: false,
     attributes: "",
     submenu: [
       {
-        id: 3.2,
+        id: 4.2,
         title: Categories["google-maps"].name,
         newTab: false,
         path: "/blog/google-maps"
       },
       {
-        id: 3.3,
+        id: 4.3,
         title: Categories["google-ads"].name,
         newTab: false,
         path: "/blog/google-ads"
       },
       {
-        id: 3.4,
+        id: 4.4,
         title: Categories["marketing"].name,
         newTab: false,
         path: "/blog/marketing"
       },
       {
-        id: 3.1,
+        id: 4.1,
         title: Categories["mindset"].name,
         newTab: false,
         path: "/blog/mindset"
       },
       {
-        id: 3.5,
+        id: 4.5,
         title: Categories["seo"].name,
         newTab: false,
         path: "/blog/seo"
@@ -93,7 +87,7 @@ const menuData: Menu[] = [
     ]
   },
   {
-    id: 3,
+    id: 5,
     title: "私たちは誰？",
     newTab: false,
     attributes: "",
