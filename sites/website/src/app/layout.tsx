6322daae344f544bd1cc3385224fd1feb0066240
tsntsumi <tsutsumi.kikuo@gmail.com>
2024-02-28 14:19:20 +0900

    Fix invalid hook  error
    フックを関数ボディーのトップ以外から呼び出しているというエラー。
    ３日間の試行錯誤の上、ルートレイアウト関数を定義した LAYOUT.JSファイルで
    metadata を定義していたのが原因だった。
    
    PAGE.JSに移動したら発生しなくなった。
    
    PAGE.JSは、'use client' を宣言してクライアントコンポーネントに
    することがよくあったので、PAGE.JSを避けていた。
    
    metadata をPAGE.JSに定義するのは当たり前で、LAYOUTに定義するような
    うっかり者は私くらいだ。
    
    そのせいでドキュメントには、そんな注意をしていない。

--- sites/website/src/app/layout.tsx
+++ sites/website/src/app/layout.tsx
@@ -1,20 +1,6 @@
-import type { Metadata } from "next"
 import "./globals.css"
-
-// const inter = Inter({ subsets: ["latin"] })
-export const metadata: Metadata = {
-  title: {
-    default: "Alizza ",
-    template: "%s | まきせ鍼灸整骨院"
-  },
-  authors: [{ name: "TSUTSUMI Kikuo", url: "https://www.alizza-ideal.com" }],
-  creator: "TSUTSUMI Kikuo",
-  publisher: "Alizza Ideal",
-  description: `慢性のしびれ・痛みで毎日が辛い・・・、
-そんな医師もお手上げのしびれ・痛みをたった数回で改善できて再発しないとしたら？
-ずっと改善されず、もう諦めているなら一度だけわたしを信じて詳細を確認してください。
-  `
-}
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children
@@ -23,7 +9,7 @@ export default function RootLayout({
 }): React.ReactNode {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <body className={`dark:bg-black`}>
+      <body className={`dark:bg-black ${inter.className}`}>
         <div>{children}</div>
       </body>
     </html>
--- sites/website/src/app/page.tsx
+++ sites/website/src/app/page.tsx
@@ -1,11 +1,27 @@
-import Header from "@/components/Header"
-import Hero from "@/components/Hero"
-import Footer from "@/components/Footer"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: {
+    template: "% | ALIZZA IDEAL 公式サイト",
+    default: "ALIZZA IDEAL 公式",
+    absolute:
+      "コーチ、ヒーラー、セラピストがクライアントに出会うなら ALIZZA IDEALの導線確保トレーニング"
+  },
+  description: `
+コーチ、ヒーラー、セラピスト、サロンオーナーになり、
+他人に貢献するために起業・開業・副業を始める人が増えています。
+しかし、だれもが自分を必要とするクライアントに出会えているわけではありません。
+Alizza Idealでは、あなたがクライアントに出会うための導線を確保するトレーニングサービスを提供しています。
+このトレーニングサービスを受けることで、
+あなたは理想のクライアント(YOUR IDEALs)に出会い、彼らの目標達成に貢献できるようになります。
+そして、あなた自身もゴールを達成し、充実した毎日を送ることになります。
+`
+}
 
 export default function Page() {
   return (
     <>
-      <div>これは本文ではありません</div>
+      <div>これは本文ではありませんヨ</div>
     </>
   )
 }
