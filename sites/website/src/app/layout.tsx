796457f31146c98fc7a85aa0df3472f39efb186b
tsntsumi <tsutsumi.kikuo@gmail.com>
2024-02-28 09:52:43 +0900

    Update to story-blog content from makise's one

--- sites/website/src/app/layout.tsx
+++ sites/website/src/app/layout.tsx
@@ -1,30 +1,18 @@
-import { Inter } from "next/font/google"
-import { Suspense } from "react"
-import Script from "next/script"
-import GoogleTag from "@/lib/googleTag"
-import Lines from "@/components/Lines"
-import ToasterContext from "@/app/context/ToastContext"
-import ScrollToTop from "@/components/ScrollToTop"
 import type { Metadata } from "next"
-
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
-
+// const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: {
-    template: "%s | Alizza Ideal",
-    default: "Alizza Ideal",
-    absolute: "Alizza Ideal 公式 Web サイト"
+    default: "Alizza ",
+    template: "%s | まきせ鍼灸整骨院"
   },
   authors: [{ name: "TSUTSUMI Kikuo", url: "https://www.alizza-ideal.com" }],
   creator: "TSUTSUMI Kikuo",
   publisher: "Alizza Ideal",
-  description: `
-これから在宅で、コンサル、コーチング、セラピスト、トレーナーとして起業・副業を始める個人に向けて、
-これまで経験してきた、ネットで見つかる・選ばれるための施策とアイデアを提供します。
-❖もしあなたが、クライアント獲得に悩んでいるなら❖もっとクライアントを増やして成長して行きたいなら、
-是非内容を確認してください。
+  description: `慢性のしびれ・痛みで毎日が辛い・・・、
+そんな医師もお手上げのしびれ・痛みをたった数回で改善できて再発しないとしたら？
+ずっと改善されず、もう諦めているなら一度だけわたしを信じて詳細を確認してください。
   `
 }
 
@@ -35,15 +23,8 @@ export default function RootLayout({
 }): React.ReactNode {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <Script src="https://polyfill.io/v3/polyfill.min.js?features=default"></Script>
-      <Suspense fallback={<div></div>}>
-        <GoogleTag />
-      </Suspense>
-      <body className={`dark:bg-black ${inter.className}`}>
-        <Lines />
+      <body className={`dark:bg-black`}>
         <div>{children}</div>
-        <ToasterContext />
-        <ScrollToTop />
       </body>
     </html>
   )
--- sites/website/src/app/page.tsx
+++ sites/website/src/app/page.tsx
@@ -5,9 +5,7 @@ import Footer from "@/components/Footer"
 export default function Page() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Footer />
+      <div>これは本文ではありません</div>
     </>
   )
 }
