e9a3202470a5f2fc4dd56168c2ddf6a86c455b65
tsntsumi <tsutsumi.kikuo@gmail.com>
2024-02-28 15:08:03 +0900

    Add ToasterContext, ScrollToTop for convinient

--- sites/website/src/app/layout.tsx
+++ sites/website/src/app/layout.tsx
@@ -1,3 +1,8 @@
+import ToasterContext from "@/app/context/ToastContext"
+import ScrollToTop from "@/components/ScrollToTop"
+import { Suspense } from "react"
+import GoogleTag from "@/lib/googleTag"
+
 import "./globals.css"
 import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
@@ -9,8 +14,13 @@ export default function RootLayout({
 }): React.ReactNode {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <Suspense ballback={<></>}>
+        <GoogleTag />
+      </Suspense>
       <body className={`dark:bg-black ${inter.className}`}>
+        <ToasterContext />
         <div>{children}</div>
+        <ScrollToTop />
       </body>
     </html>
   )
